2db70a34e02988a166707a9077e72189ebbab113
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-16 14:36:48 +0200

    snack_3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,3 +2,14 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+let newArray = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    const numberPlusOne = number + 1;
+    
+    newArray.push(numberPlusOne);
+};
+
+console.log(newArray);
