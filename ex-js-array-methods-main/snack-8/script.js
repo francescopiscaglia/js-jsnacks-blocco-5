ac192fa2f820c75f5e6e97998d33171e7d08f0e2
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-16 15:24:35 +0200

    snack_8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,29 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+let classNumber;
+
+const specificClass = students.filter(element => {
+  if (element.class === "3C") {
+    classNumber = element.class
+    return true;
+  };
+  return false;
+});
+console.log(classNumber);
+
+
+// for loop
+let number;
+
+for (let i = 0; i < students.length; i++) {
+  const singleStudent = students[i];
+
+  if (singleStudent.class === "3C") {
+    number = singleStudent.class;
+    break;
+  };
+};
+
+console.log(number);
